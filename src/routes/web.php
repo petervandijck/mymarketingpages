60542f5c32167406135f4c1d975fc24005328ee7
petervandijck <petervandijck@gmail.com>
2025-03-06 12:50:38 +0100

    routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,21 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-/**
+/*
  * Marketing pages
- *
- * /                    -> loggedout homepage
- * /features
- * /why                 -> use cases and differentiators
- * /pricing
- * /about/team
- * /about/vision
- * /resources/faq
- * /resources/support
- * /resources/contact
- * /legal/toc
- * /legal/privacy
  */
-Route::get('/', function () {
-    return view('marketing.pages.home');
-})->name('marketing.home');
+
+Route::view('/', 'marketing.pages.home'); // Loggedout homepage
+Route::view('/features', 'marketing.pages.');
+Route::view('/why', 'marketing.pages.why'); // use cases and differentiators
+Route::view('/pricing', 'marketing.pages.pricing');
+Route::view('/about/team', 'marketing.pages.about.team');
+Route::view('/about/vision', 'marketing.pages.about.vision');
+Route::view('/resources/faq', 'marketing.pages.resources.faq');
+Route::view('/resources/support', 'marketing.pages.resources.support');
+Route::view('/resources/contact', 'marketing.pages.resources.contact');
+Route::view('/legal/toc', 'marketing.pages.legal.toc');
+Route::view('/legal/privacy', 'marketing.pages.legal.privacy');
+Route::view('/testing/everything', 'marketing.pages.testing.everything');
